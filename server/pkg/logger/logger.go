564b19554e14d5aee5f9e91eaf8a996e529998bf
Victor Wu <wuvictor95@gmail.com>
2023-05-19 10:03:55 -0700

    add logger

---
+++ server/pkg/logger/logger.go
@@ -0,0 +1,74 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+type loggerKey int
+
+const LoggerKey loggerKey = 0
+
+type Logger interface {
+	With(args ...interface{}) Logger
+
+	WithoutCaller() Logger
+	Debug(args ...interface{})
+	// Info uses fmt.Sprint to construct and log a message at INFO level
+	Info(args ...interface{})
+	// Error uses fmt.Sprint to construct and log a message at ERROR level
+	Error(args ...interface{})
+	// Fatal uses fmt.Sprint to construct and log a message, then calls os.Exit.
+	Fatal(args ...interface{})
+
+	// Debugf uses fmt.Sprintf to construct and log a message at DEBUG level
+	Debugf(format string, args ...interface{})
+	// Infof uses fmt.Sprintf to construct and log a message at INFO level
+	Infof(format string, args ...interface{})
+	// Errorf uses fmt.Sprintf to construct and log a message at ERROR level
+	Errorf(format string, args ...interface{})
+	// Fatalf uses fmt.Sprintf to construct and log a message, then calls os.Exit.
+	Fatalf(format string, args ...interface{})
+}
+
+type logger struct {
+	*zap.SugaredLogger
+}
+
+// New creates a new logger using the default configuration
+func New() Logger {
+	l, _ := zap.NewProduction()
+	return NewSugar(l)
+}
+
+// Returns a logger and observer for testing
+func NewTest() (Logger, *observer.ObservedLogs) {
+	observedLogs, logObserver := observer.New(zap.InfoLevel)
+	testLogger := zap.New(observedLogs)
+	return NewSugar(testLogger), logObserver
+}
+
+// NewSugar returns a SugaredLogger and implements the Logger interface
+func NewSugar(l *zap.Logger) Logger {
+	return &logger{l.Sugar()}
+}
+
+// With returns a logger based off the root logger and decorates it with the arguments
+func (l *logger) With(args ...interface{}) Logger {
+	return &logger{l.SugaredLogger.With(args...)}
+}
+
+// WithoutCaller returns a logger that does not output the caller field and location of the calling code.
+func (l *logger) WithoutCaller() Logger {
+	return &logger{l.SugaredLogger.WithOptions(zap.WithCaller(false))}
+}
+
+// FromContext returns a logger from context. If none found, instantiate a new logger
+func FromContext(ctx context.Context) Logger {
+	if l, ok := ctx.Value(LoggerKey).(Logger); ok {
+		return l
+	}
+	return New()
+}
---
+++ server/pkg/logger/logger_test.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	l := New()
+	assert.NotNil(t, l)
+}
+
+func Test_logger_With(t *testing.T) {
+	l, observer := NewTest()
+	l = l.With("requestID", "123")
+	l.Info("Logging with request ID field appended")
+	logs := observer.All()
+	entry := logs[0]
+	fields := entry.Context
+	assert.Equal(t, "requestID", fields[0].Key)
+	assert.Equal(t, "123", fields[0].String)
+}
+
+func Test_logger_WithoutCaller(t *testing.T) {
+	l, observer := NewTest()
+	l = l.WithoutCaller()
+	l.Info("Logging without caller")
+	logs := observer.All()
+	entry := logs[0]
+	assert.Equal(t, false, entry.Caller.Defined)
+}
+
+func Test_logger_FromContext(t *testing.T) {
+	t.Run("context with logger", func(t *testing.T) {
+		l, observer := NewTest()
+		l = l.With("customArgKey", "customArgValue")
+		ctx := context.WithValue(context.Background(), LoggerKey, l)
+		loggerFromCtx := FromContext(ctx)
+		assert.NotNil(t, loggerFromCtx)
+		loggerFromCtx.Info("Logging to test for customArg field")
+		logs := observer.All()
+		entry := logs[0]
+		fields := entry.Context
+		assert.Equal(t, "customArgKey", fields[0].Key)
+	})
+
+	t.Run("context without logger", func(t *testing.T) {
+		loggerFromCtx := FromContext(context.Background())
+		assert.NotNil(t, loggerFromCtx)
+	})
+}
