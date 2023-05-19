453aab90676e5b5fb06eb296d9f0445002ca743b
Victor Wu <wuvictor95@gmail.com>
2023-05-18 22:43:24 -0700

    add graceful shutdown

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Wave-95/boards/server/db"
 	"github.com/Wave-95/boards/server/internal/api/user"
@@ -23,8 +28,33 @@ func main() {
 	}
 	defer db.Close()
 
+	// setup server
 	r := chi.NewRouter()
-	userAPI := user.NewAPI(user.NewService(user.NewRepository(db)))
+	server := http.Server{Addr: ":8080", Handler: buildHandler(r, db)}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not start server: %s", err)
+		}
+	}()
+
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced shutdown: %s", err)
+	}
+}
+
+func buildHandler(r chi.Router, db *db.DB) chi.Router {
+	// register user handlers
+	userRepo := user.NewRepository(db)
+	userService := user.NewService(userRepo)
+	userAPI := user.NewAPI(userService)
 	userAPI.RegisterHandlers(r)
-	http.ListenAndServe(":8080", r)
+
+	return r
 }
