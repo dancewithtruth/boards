a745b3533247b5c4f1f91501a03de2af6ad214fe
Victor Wu <wuvictor95@gmail.com>
2023-06-10 15:22:23 -0700

    backend-core: order test sql data

--- backend-core/testdata/2_boards.sql
+++ backend-core/testdata/2_boards.sql
@@ -4,7 +4,6 @@ VALUES
     ('b9e95ae4-9c3f-412f-8b3b-201bd7083fc1', 'Board 1', 'Description for Board 1', 'd0865843-8494-4d6a-b9be-5c8f7d0e568f', '2023-05-29 10:30:00', '2023-05-29 10:30:00'),
     ('2f9e7ef7-0725-4327-9253-129ff76dbdf1', 'Board 2', 'Description for Board 2', 'd0865843-8494-4d6a-b9be-5c8f7d0e568f', '2023-05-29 11:00:00', '2023-05-29 11:00:00'),
     ('9684c49d-8a14-4dbf-98f4-11a2dd1a6db2', 'Board 3', 'Description for Board 3', 'd0865843-8494-4d6a-b9be-5c8f7d0e568f', '2023-05-29 11:30:00', '2023-05-29 11:30:00'),
-
     ('dce8a3a4-80ad-4a69-a849-91c8c2a7d260', 'Board 1', 'Description for Board 1', '3c85c78d-d6a2-48c1-aec0-7fe2a9e2d8db', '2023-05-29 11:00:00', '2023-05-29 11:00:00'),
     ('60357c10-940a-4e85-a28a-7a9d6f6f9a44', 'Board 2', 'Description for Board 2', '3c85c78d-d6a2-48c1-aec0-7fe2a9e2d8db', '2023-05-29 11:30:00', '2023-05-29 11:30:00'),
-    ('30ae9e99-3cfd-4c35-8443-370ed7b857ae', 'Board 3', 'Description for Board 3', '3c85c78d-d6a2-48c1-aec0-7fe2a9e2d8db', '2023-05-29 12:00:00', '2023-05-29 12:00:00'),
+    ('30ae9e99-3cfd-4c35-8443-370ed7b857ae', 'Board 3', 'Description for Board 3', '3c85c78d-d6a2-48c1-aec0-7fe2a9e2d8db', '2023-05-29 12:00:00', '2023-05-29 12:00:00');
