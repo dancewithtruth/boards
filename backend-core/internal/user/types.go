1469b982459c23008bafd8f2dcb52237b8afa9f8
Victor Wu <wuvictor95@gmail.com>
2023-06-12 08:15:31 -0700

    user: extend name to 24

--- backend-core/internal/user/types.go
+++ backend-core/internal/user/types.go
@@ -7,7 +7,7 @@ import (
 
 // CreateUserInput defines the structure for requests to create a new user.
 type CreateUserInput struct {
-	Name     string  `json:"name" validate:"required,min=2,max=12"`
+	Name     string  `json:"name" validate:"required,min=2,max=24"`
 	Email    *string `json:"email" validate:"omitempty,email,required"`
 	Password *string `json:"password" validate:"omitempty,min=8"`
 	IsGuest  bool    `json:"is_guest" validate:"omitempty,required"`
