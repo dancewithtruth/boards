5d8e1ce45c7507dbba49a7e058ea853e9527df79
Victor Wu <wuvictor95@gmail.com>
2023-08-03 09:01:25 -0700

    frontend: update toast msg

--- frontend/src/components/dnd/board.tsx
+++ frontend/src/components/dnd/board.tsx
@@ -71,7 +71,7 @@ export const Board: FC<BoardProps> = ({ board, postGroups: initialPostGroups }) 
     // Scroll to the top left portion of the page
     window.scrollTo(0, 0);
     // Usability message
-    toast.info('Double-click anywhere on the grid!', { autoClose: false });
+    toast.info('Double-click anywhere to make a post!', { autoClose: false });
   }, []);
 
   // Expands the board based on post locations
